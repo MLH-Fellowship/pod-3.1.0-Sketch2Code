b7f288e5e7ca3c577afbb11a5dab00713d6fb01a
gokulnair2001 <gokulnair.2001@gmail.com>
2021-08-03 23:00:59 +0530

    additional element support

--- App/Swift Co-Pilot/Swift Co-Pilot/Controller/ViewController.swift
+++ App/Swift Co-Pilot/Swift Co-Pilot/Controller/ViewController.swift
@@ -11,12 +11,13 @@ import VisionKit
 import CoreML
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var scanBtn: UIButton!
     
     private var request = VNRecognizeTextRequest(completionHandler: nil)
     private let buttonDetector = buttonShapeDetector()
     private var selectedImage = UIImageView()
+    private var elementDetected = String()
     static var resultSnippet = [String()]
     
     override func viewDidLoad() {
@@ -24,13 +25,13 @@ class ViewController: UIViewController {
         
         scanBtn.layer.cornerRadius = 14
     }
-
+    
     @IBAction func scanButton(_ sender: Any) {
         showDataInputType()
     }
     
     func showDataInputType() {
-        let alert = UIAlertController(title: "Input types", message: "Select a input type", preferredStyle: .actionSheet)
+        let alert = UIAlertController(title: "Input mode", message: "Select a input mode", preferredStyle: .actionSheet)
         alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
             let scannerViewController = VNDocumentCameraViewController()
             scannerViewController.delegate = self
@@ -70,9 +71,7 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
     private func setupVisionTextRecognition(image: UIImage?){
         //indicator.startAnimating()
         var textString = ""
-        //Handler
-        //Request
-        //Process Request
+        
         request = VNRecognizeTextRequest(completionHandler: { (request, error) in
             guard let observations = request.results as? [VNRecognizedTextObservation] else{fatalError("Recived invalid observation")}
             
@@ -131,26 +130,36 @@ extension ViewController: VNDocumentCameraViewControllerDelegate {
 //MARK:- ML Model Application
 extension ViewController {
     func identifyElement(text: String) {
-        let inputText = text.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        elementDetected = text.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        switch inputText {
+        switch elementDetected {
         case "button":
             buttonImageClassifier()
-            print("button detected")
             return
         case "label":
-            print("label detected")
             codeSnippet(snippetNo: 2)
             return
         case "textfield":
-            print("textfield detected")
             codeSnippet(snippetNo: 3)
+            return
+        case "textview":
+            codeSnippet(snippetNo: 4)
+            return
+        case "switch":
+            codeSnippet(snippetNo: 5)
+            return
+        case "view":
+            buttonImageClassifier()
+            return
+        case "segment":
+            codeSnippet(snippetNo: 7)
             return
         default:
             print("error")
             return
         }
     }
+    //MARK:- Shape Classifier
     
     func buttonImageClassifier(){
         
@@ -167,17 +176,19 @@ extension ViewController {
             for result in prediction{
                 let res = result.classLabel
                 
-                if res == "circles"{
+                if res == keys.valueOf.circle{
+                    snippetAllocator(with: res)
                     print("ml model detected circle")
                 }
-                else if res == "squares"{
-                    codeSnippet(snippetNo: 1)
+                else if res == keys.valueOf.square{
+                    snippetAllocator(with: res)
                     print("ml model detected square")
                 }
-                else if res == "triangles"{
+                else if res == keys.valueOf.triangle{
                     print("ml model detected triangle")
                 }
                 else {
+                    // Provide default code for identified UIElement
                     print("ml model detected nil")
                 }
             }
@@ -187,11 +198,45 @@ extension ViewController {
         }
     }
     
+    //MARK:- Snippet allocator
+    /// Providing Snippet according to the element type and its shape detected
+    func snippetAllocator(with shape: String) {
+        switch elementDetected {
+        case "button":
+            if shape == keys.valueOf.square {
+                codeSnippet(snippetNo: 1)
+            }
+            else if shape == keys.valueOf.circle{
+                // code snippet with button of circular shape
+            }
+            else if shape == keys.valueOf.triangle {
+                // code snippet with button of triangular shape
+            }
+            return
+        case "view":
+            if shape == keys.valueOf.square {
+                codeSnippet(snippetNo: 6)
+            }
+            else if shape == keys.valueOf.circle {
+                
+                // code snippet with view of circular shape
+            }
+            else if shape == keys.valueOf.triangle {
+                // code snippet with button of triangular shape
+            }
+            return
+        default:
+            break
+        }
+    }
+    
+    //MARK:-  Code Snippet Provider
+    
     func codeSnippet(snippetNo: Int) {
         switch snippetNo {
         case 1:
             ViewController.resultSnippet =
-               [ """
+                [ """
             let button = UIButton()
             button.frame = CGRect(x: view.center.x, y: view.center.y, width: 100, height: 100)
             button.setTitle("", for: .normal)
@@ -199,7 +244,7 @@ extension ViewController {
             button.layer.borderWidth = 0
             button.layer.borderColor = UIColor.clear.cgColor
 """,
-                 """
+                  """
                               Button(action:{
                                   //do something when button is tapped
                               }){
@@ -212,23 +257,23 @@ extension ViewController {
   """]
         case 2:
             ViewController.resultSnippet =
-               [ """
+                [ """
             let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
             label.center = CGPoint(x: 160, y: 285)
             label.textAlignment = .center
             label.text = "I'm a test label"
             self.view.addSubview(label)
 """,
-                 """
+                  """
               Label("SwiftUI Label", systemImage: "book.fill")
                 .labelStyle(TitleOnlyLabelStyle())
   """]
         case 3:
             ViewController.resultSnippet =
-               [ """
+                [ """
             UITextField
 """,
-                 """
+                  """
               struct ContentView: View {
                   @State var username: String = ""
                   
@@ -239,6 +284,38 @@ extension ViewController {
                   }
               }
   """]
+        case 4:
+            ViewController.resultSnippet =
+                ["""
+UITextView
+""",
+                 """
+SwiftUI code for UItextView
+"""]
+        case 5:
+            ViewController.resultSnippet =
+                ["""
+UISwitch
+""",
+                 """
+SwiftUI code for UISwitch
+"""]
+        case 6:
+            ViewController.resultSnippet =
+                ["""
+UIView
+""",
+                 """
+UIView is not present in SwiftUI
+"""]
+        case 7:
+            ViewController.resultSnippet =
+                ["""
+UISegment Control
+""",
+                 """
+SwiftUI code for UISegment Control
+"""]
         default:
             break
         }
