1d60bb44d1771eca19b974834b866cbc26e08249
Amin Zamani <abz@email.unc.edu>
2021-08-10 07:40:56 -0400

    add firebase

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from collections import defaultdict
 from sqlalchemy.dialects import registry
 registry.register("cockroachdb", "cockroachdb.sqlalchemy.dialect",
                   "CockroachDBDialect")
+import firebase_admin
+from firebase_admin import credentials, db
 from history_actions import HistoryActions
 
 app = Flask(__name__)
@@ -12,6 +14,8 @@ app.secret_key = "asdfasfdasfdsafasddfsadfasdfsadfdas"
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 LIMIT = 10
 database = HistoryActions(os.environ['DATABASE_URL'])
+cred = credentials.Certificate(json.loads(os.environ['FIREBASE_KEY']))
+firebase_admin.initialize_app(cred)
 
 userHistory = defaultdict(list)
 
@@ -24,15 +28,21 @@ def history():
     ip = request.environ['REMOTE_ADDR']
     print("ip====",ip)
     if request.method == 'GET':
-        res = userHistory["ip"] if "ip" in userHistory else []
+        res = db.reference('/ip').get() if "ip" in db.reference("/").get(shallow=True) else []
         return jsonify({'ip_address': res}), 200
     elif request.method == 'POST':
         title = request.get_json().get('title', '')
         code = request.get_json().get('code', '')
         #session["ip"].append({'title': title, 'code': code})
-        userHistory["ip"].append({'title': title, 'code': code})
-        while len(userHistory["ip"]) > LIMIT:
-            userHistory["ip"].pop(0)
+        #userHistory["ip"].append({'title': title, 'code': code})
+        db.reference('/ip').update({'title': title, 'code': code})
+        sz = len(db.reference("/ip").get())
+        if sz > LIMIT:
+            diff = sz - LIMIT
+            keys = db.reference("/ip").get(shallow=True)
+            for v in keys:
+                if diff == 0: break
+                db.reference("/ip/" + v).delete()
         return jsonify(success=True), 200
 if __name__ == '__main__':
     app.run(debug=True) #debug=True so that caching doesn't occur
